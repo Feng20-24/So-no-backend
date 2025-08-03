b75d350c926273d25efbff626e22f31c23a18c29
Feng20-24 <Phong.PT235987@sis.hust.edu.vn>
2025-08-03 09:02:30 +0700

    Revert "first commit"
    This reverts commit 2fa1211f3c6994dc9e1fdd0363af30019edbf874.

--- routers/transaction.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {TransactionController} from '../controllers/TransactionController.js';
-
-const router = express.Router();
-
-router.get('/', TransactionController.getMyTransactions);
-router.post('/', TransactionController.createTransaction);
-
-export default router;
