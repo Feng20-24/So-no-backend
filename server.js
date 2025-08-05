e7724e0aa12cda2137553f8b78bfbac83a4132a2
Feng20-24 <Phong.PT235987@sis.hust.edu.vn>
2025-08-05 17:18:31 +0700

    :sparkles: Add the limit for data passing (for images - hash64)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import {connectDb} from './database2/database-orm.js';
-import './models-orm-sequelize/index.js'; // Import models to register them with Sequelize
+import './models-orm-sequelize/index.js';
 import router from './routers/index.js';
 import './middlewares/auth.js';
 import schedule from './cron.js';
@@ -18,10 +18,9 @@ await connectDb();
 schedule.start();
 
 app.use(cors({
-    // origin: 'http://localhost:5173',
     credentials: true
 }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
 router(app);
